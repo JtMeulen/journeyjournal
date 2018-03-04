be32d0491405a5acc746c6e6fe62ff9ae502e6d3
JoeyM <joeytermeulen@gmail.com>
2018-03-04 16:42:51 +0100

    Add some more routes

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ app.get('/newzealand', function(req, res){
     res.render('newzealand');
 });
 
+app.get('/srilanka', function(req, res){
+    res.send('Sri Lanka');
+});
+
+app.get('/southafrica', function(req, res){
+    res.send('southafrica');
+});
+
+app.get('/usa', function(req, res){
+    res.send('USA');
+});
+
+app.get('/malaysia', function(req, res){
+    res.send('malaysia');
+});
+
 app.listen(3000, function(){
     console.log("APP RUNNING on PORT 3000");
 });
